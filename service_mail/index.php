7ff863c48fd60397caab68a1c103e4afaa7ff13c
João Miguel <jrsmiguel@outlook.pt>
2018-03-01 11:50:41 +0000

    Update to merge

--- service_mail/index.php
+++ service_mail/index.php
@@ -2,7 +2,7 @@
 <html>
   <body>
 
-    <form>
+    <form action="mail.php0" method="POST">
       Sender token:<br>
       <input type="text" name="token">
       <br>
@@ -19,49 +19,5 @@
       <br>
       <input type="submit" value="Send">
     </form>
-
-    <?php
-      // Import PHPMailer classes into the global namespace
-      // These must be at the top of your script, not inside a function
-      use PHPMailer\PHPMailer\PHPMailer;
-      use PHPMailer\PHPMailer\Exception;
-
-      //Load composer's autoloader
-      require 'Exception.php';
-      require 'PHPMailer.php';
-      require 'SMTP.php';
-
-      $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-      try {
-        //Server settings
-        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = '172.21.0.2';                           // Specify main and backup SMTP servers
-        $mail->Port = 25;                                    // TCP port to connect to
-
-        //Recipients
-        $mail->setFrom('jrsmiguel@outlook.pt', 'João Rui');
-        $mail->addAddress('jrsmiguel@outlook.pt', 'Joe User');     // Add a recipient
-        //$mail->addAddress('ellen@example.com');               // Name is optional
-        //$mail->addReplyTo('info@example.com', 'Information');
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
-
-        //Attachments
-        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-        //Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        $mail->send();
-        echo 'Message has been sent';
-      } catch (Exception $e) {
-        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-      }
-      ?>
   </body>
 </html>
