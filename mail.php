287b0f46ed147143534c8d958900d1efba2bb11f
Bruno Chianca Ferreira <brunobcf@gmailcom>
2018-03-01 10:09:04 +0000

    First

---
+++ mail.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <body>
+
+    <form>
+      Sender token:<br>
+      <input type="text" name="token">
+      <br>
+      To:<br>
+      <input type="email" name="emailto">
+      <br>
+      Subject:<br>
+      <input type="text" name="subject">
+      <br>
+      Message:<br>
+      <textarea name="message" rows="20" cols="60">
+      Enter message here.
+      </textarea> 
+      <br>
+      <input type="submit" value="Send">
+    </form>
+
+  </body>
+</html>
