e79ba31a7f56c10bc75793291622d07a8993a0c9
João Miguel <jrsmiguel@outlook.pt>
2018-03-01 11:42:27 +0000

    Updated auth page

--- service_auth/index.php
+++ service_auth/index.php
@@ -1,16 +1,30 @@
+<!DOCTYPE html>
 <html>
-<body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Auth Service</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+  </head>
+  <body>
+    <div class="wrap">
+      <div class="avatar">
+        <img src="https://digitalnomadsforum.com/styles/FLATBOOTS/theme/images/user4.png">
+        <form>
+          </div>
+            <input type="text" placeholder="username" required>
+          <div class="bar">
+          <i></i>
+        <form>
+      </div>
+      <input type="password" placeholder="password" required>
+      <a href="" class="fl"></a>
+      <button>Sign in</button>
+    </div>
 
-<form action="index.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
-<?php
-echo $_POST["name"];
-echo $_POST["email"];
-?>
-
-</body>
+    <?php
+      echo $_POST["name"];
+      echo $_POST["email"];
+    ?>
+  </body>
 </html>
