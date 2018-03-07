dd4124f19a6cb3c3e79362b54e0030153727569d
brunobcfum <36694413+brunobcfum@users.noreply.github.com>
2018-03-07 14:05:29 +0000

    Added redirection

--- service_auth/login.php
+++ service_auth/login.php
@@ -42,12 +42,14 @@ if($r->rowCount() !== 0){
       $res = $conn->query($createToken);
       if($res !== false){
         $res = $conn->query($updateToken);
+        header("Location: http://localhost:9000/mail?token=".$token);
         echo "Token a ser utilizador no mail: $token\n";
       }
       else{
         $updateUser = 'UPDATE tokens SET tokenid = '.$token.' WHERE userid = '.$row['userid'].';';
         $res = $conn->query($updateUser);
         $res = $conn->query($updateToken);
+        header("Location: http://localhost:9000/mail?token=".$token);
         echo "Token a ser utilizador no mail: $token\n";
       }
     }
